4e4a1bc63c2c136e7a167adb770888441c24f844
vspaz <vladimir.edelman@gmail.com>
2023-11-10 21:39:56 +0100

    add http stub

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module random-user-generator
 
 go 1.21.0
+
+require github.com/vspaz/goat v1.3.2
+
+require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/vspaz/simplelogger v0.1.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+)
--- internal/api/api.go
+++ internal/api/api.go
@@ -1,1 +1,25 @@
 package api
+
+import (
+	"github.com/vspaz/goat/pkg/ghttp"
+	"log"
+)
+
+func GetRandomUserInfo() *ghttp.Response {
+	client := ghttp.NewClientBuilder().
+		WithHost("https://randomuser.me/api").
+		WithUserAgent("randomuser").
+		WithRetry(3, []int{500, 503}).
+		WithDelay(0.5).
+		WithResponseTimeout(10.0).
+		WithConnectionTimeout(2.0).
+		WithHeadersReadTimeout(2.0).
+		WithLogLevel("info").
+		Build()
+
+	resp, err := client.DoGet("/get", nil, nil)
+	if err != nil {
+		log.Fatalf("failed to fetch user info")
+	}
+	return resp
+}
