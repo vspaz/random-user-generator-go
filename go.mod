63a70f473db1c3d23926cedb8718e3b69d8267b4
vspaz <vladimir.edelman@gmail.com>
2023-11-11 17:54:57 +0100

    add 'TestRandomUserData_Generate'

--- go.mod
+++ go.mod
@@ -2,10 +2,16 @@ module random-user-generator
 
 go 1.21.0
 
-require github.com/vspaz/goat v1.3.2
+require (
+	github.com/stretchr/testify v1.8.4
+	github.com/vspaz/goat v1.3.2
+)
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/vspaz/simplelogger v0.1.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/user/user_test.go
+++ pkg/user/user_test.go
@@ -1,1 +1,66 @@
 package user
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/vspaz/goat/pkg/ghttp"
+	"net/http"
+	"net/http/httptest"
+	"random-user-generator/internal/api"
+	"random-user-generator/internal/configuration"
+	"testing"
+)
+
+const (
+	seed      = "foobar"
+	userCount = 1
+)
+
+var endpoint = fmt.Sprintf("/api?inc=name&results=%d&seed=%s", userCount, seed)
+
+func startTestServer(t *testing.T) *httptest.Server {
+	payload := Users{
+		Results: []User{
+			{
+				Name: Name{
+					Title: "Mr",
+					First: "John",
+					Last:  "Doe",
+				}},
+		},
+	}
+	return httptest.NewServer(
+		http.HandlerFunc(
+			func(writer http.ResponseWriter, request *http.Request) {
+				assert.Equal(t, endpoint, request.URL.String())
+				writer.WriteHeader(http.StatusOK)
+				encodedBody, _ := json.Marshal(payload)
+				if _, err := writer.Write(encodedBody); err != nil {
+					t.Fatal(err)
+				}
+			},
+		),
+	)
+}
+
+func TestRandomUserData_Generate(t *testing.T) {
+	server := startTestServer(t)
+	defer server.Close()
+	apiClient := &api.ApiClient{
+		HttpClient: ghttp.NewClientBuilder().WithHost(server.URL).Build(),
+		Config:     configuration.GetConfig(),
+	}
+	randomUser := &RandomUserData{
+		seed:      seed,
+		userCount: userCount,
+		latinOnly: false,
+		namesOnly: true,
+		apiClient: apiClient,
+	}
+	randomData, err := randomUser.Generate()
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(randomData))
+	assert.Equal(t, "John", randomData[0].Name.First)
+	assert.Equal(t, "Doe", randomData[0].Name.Last)
+}
