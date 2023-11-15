31b2d5fa83e42b98b9f6342fe72cd83a813e5909
vspaz <vladimir.edelman@gmail.com>
2023-11-15 22:23:13 +0100

    extend the example

--- examples/main.go
+++ examples/main.go
@@ -7,7 +7,13 @@ import (
 func main() {
 	randomUserData := user.NewRandomUserData("foobarbaz", 1, false, true)
 	data, _ := randomUserData.Generate()
-	userName := data[0].Name
-	print(userName.First)
-	print(userName.Last)
+	randomUser := data[0]
+	println(randomUser.Name.First)
+	println(randomUser.Name.Last)
+	println(randomUser.Location.Country)
+	println(randomUser.Location.City)
+	println(randomUser.Location.Street.Name)
+	println(randomUser.Gender)
+	println(randomUser.Nat)
+	println(randomUser.Email)
 }
